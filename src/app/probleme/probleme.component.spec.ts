299fb3930e912b079451ef35c11f49232b8101da
choupinc <1878653@cmontmorency.qc.ca>
2021-03-27 14:43:35 -0400

    tp7

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
@@ -8,9 +9,10 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProblemeComponent ]
+      imports: [ReactiveFormsModule],
+      declarations: [ProblemeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +24,52 @@ describe('ProblemeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Zone PRÉNOM invalide avec 2 caractères', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('2'.repeat(2));
+    errors = zone.errors || {};
+    expect(zone.valid).toBeFalsy();
+  });
+
+  it('Zone PRÉNOM valide avec 3 caractères', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('0'.repeat(3));
+    errors = zone.errors || {};
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('5'.repeat(200));
+    errors = zone.errors || {};
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('Zone PRÉNOM invalide avec aucune valeur', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('')
+    errors = zone.errors || {};
+    expect(zone.valid).toBeFalsy();
+  });
+
+  it('Zone PRÉNOM valide avec 10 espaces', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(' '.repeat(10))
+    errors = zone.errors || {};
+    expect(zone.valid).toBeTruthy();
+  });
+  it('Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+    let errors = {};
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(' 8 ')
+    errors = zone.errors || {};
+    expect(zone.valid).toBeTruthy();
+  });
+
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'Inter-probleme',
+  selector: 'stk-probleme',
   templateUrl: './probleme.component.html',
   styleUrls: ['./probleme.component.css']
 })
 export class ProblemeComponent implements OnInit {
+  problemeForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.problemeForm = this.fb.group({
+
+      prenom: ['', [Validators.minLength(3), Validators.required]]
+
+    });
+  }
+
+  save(): void {
+
   }
 
 }
